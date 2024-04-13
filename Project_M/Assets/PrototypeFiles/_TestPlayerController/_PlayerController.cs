a842e3d00809cbda48aec13c068b8f0558cc9d7f
GerhardVFX <90000584+GerryVFX@users.noreply.github.com>
2024-04-12 21:34:59 -0600

    PlayerMove

--- Project_M/Assets/PrototypeFiles/_TestPlayerController/_PlayerController.cs
+++ Project_M/Assets/PrototypeFiles/_TestPlayerController/_PlayerController.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class _PlayerController : MonoBehaviour
 {
+    public CharacterController controller;
+    private float moveZ;
+    private float moveX;
+    public float speed;
+
+    public Animator animController;
+
+    public bool aim;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,65 @@ public class _PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        moveX = Input.GetAxis("Horizontal");
+        moveZ = Input.GetAxis("Vertical");
+
+        Vector3 moveDir = new Vector3(moveX, 0, moveZ);
+        Vector3 rotationDir = new Vector3(0, moveX, 0);
+
         
+        if (aim)
+        {
+            transform.Rotate(rotationDir,Space.World);
+        }
+        else
+        {
+            controller.Move(moveDir * (speed * Time.deltaTime));
+            if (moveDir != Vector3.zero)
+            {
+                gameObject.transform.forward = moveDir;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            animController.SetBool("IsRun", true);
+            speed = 5f;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            animController.SetBool("IsRun", false);
+            speed = 3f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            aim = true;
+            animController.SetBool("Aim", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Mouse1))
+        {
+            StartCoroutine(Walkink());
+            animController.SetBool("Aim", false);
+        }
+        if(Input.GetKeyDown(KeyCode.Mouse2))animController.SetTrigger("Shoot");
+        
+        
+        if (moveX != 0 || moveZ != 0)
+        {
+            animController.SetBool("IsWalk", true);
+        }
+        else
+        {
+            animController.SetBool("IsWalk", false);
+        }
+    }
+
+    public IEnumerator Walkink()
+    {
+        yield return new WaitForSeconds(1f);
+        aim = false;
     }
 }
